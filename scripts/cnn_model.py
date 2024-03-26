88ab0932f4f1daf7aec2e9684c550e0299a23b33
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2024-03-26 17:00:30 +0530

    changes made

--- scripts/cnn_model.py
+++ scripts/cnn_model.py
@@ -27,3 +27,19 @@ classifier.add(Dense(units=1, activation="sigmoid"))
 
 # compile the model
 classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# Fitting the CNN Model to Images
+# Image Augmentation
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+training_set = train_datagen.flow_from_directory(
+    "dataset/training_set/", target_size=(64, 64), batch_size=32, class_mode="binary"
+)
+
+test_set = test_datagen.flow_from_directory(
+    "dataset/test_set/", target_size=(64, 64), batch_size=32, class_mode="binary"
+)
